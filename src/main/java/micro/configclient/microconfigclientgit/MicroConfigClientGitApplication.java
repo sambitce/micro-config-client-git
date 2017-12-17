52e1de5ad6efeab0d2867e3bd8b97c1d8f486891
sambath <sambathkumar.rsk@gmail.com>
2017-12-17 00:38:37 -0600

    initial commit

---
+++ src/main/java/micro/configclient/microconfigclientgit/MicroConfigClientGitApplication.java
@@ -0,0 +1,12 @@
+package micro.configclient.microconfigclientgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroConfigClientGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroConfigClientGitApplication.class, args);
+	}
+}
